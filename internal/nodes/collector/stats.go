1a5145ffa691b3f94d969365f2370301c6dc4207
maier <maier@users.noreply.github.com>
2020-05-20 14:17:04 -0400

    doc: copyright

--- internal/nodes/collector/stats.go
+++ internal/nodes/collector/stats.go
@@ -1,4 +1,4 @@
-// Copyright © 2019 Circonus, Inc. <support@circonus.com>
+// Copyright © 2020 Circonus, Inc. <support@circonus.com>
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 //
