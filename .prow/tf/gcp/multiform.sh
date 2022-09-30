8c1eb0ac05ba3a71a020e6680ecd5b40c71e7e87
Antonio Escalera <antonio.escalera@circonus.com>
2022-09-30 19:57:21 -0400

    added multiform script

---
+++ .prow/tf/gcp/multiform.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+
+# multiform.sh - multiplexes terraform
+
+# USAGE: $0 [COMMAND]
+# Commands:
+#
+# destroy  | destroys all resources in all workspaces
+# plan     | plans all resources in all workspaces
+# apply    | provisions all resources in all workspaces
+
+# DEPENDENCIES:
+# - yq ()
+
+DATA_FILE=./multiform.yaml
+LOG_DIR=./logs/
+FLAGS="-input=false -auto-approve"
+
+destroy() {
+  # shellcheck disable=SC2086
+  for_workspace_vars_terraform destroy ${FLAGS}
+
+#  for workspace in $(yq -r '.workspaces[].name' "${DATA_FILE}"); do
+#    workspace_vars=""
+#    for var in $(yq -r '.workspaces[] | select(.name == '"${workspace}"') | .vars[].name' "${DATA_FILE}"); do
+#      workspace_vars="${workspace_vars} -var ${var}=$(yq -r '.workspaces[] | select(.name == '"${workspace}"') | .vars[] | select(.name == "kubernetes_version") | .value' ${DATA_FILE})"
+#    done
+#
+#    mkdir -p "${LOG_DIR}/${workspace}"
+#    # shellcheck disable=SC2086
+#    TF_WORKSPACE="${workspace}" terraform destroy ${FLAGS} ${workspace_vars} &>"${LOG_DIR}/${workspace}/destroy.log" &
+#  done
+}
+
+watch_destroy() {
+  finished=false
+  while [ "${finished}" != "true" ]; do
+    adder=0
+    for workspace in $(yq -r '.workspaces[].name' "${DATA_FILE}"); do
+      if grep -q "Destroy complete" "${LOG_DIR}/${workspace}/destroy.log"; then
+        :
+      else
+        echo "Still waiting on workspace: ${workspace}"
+        (( adder++ ))
+      fi
+    done
+    if [ "${adder}" -gt 0 ]; then
+      echo "Destroy not complete, continuing to wait..."
+      sleep 5
+    else
+      echo "Destroy complete"
+      finished="true"
+    fi
+  done
+}
+
+plan() {
+  for_workspace_vars_terraform plan
+
+#  for workspace in $(yq -r '.workspaces[].name' "${DATA_FILE}"); do
+#    workspace_vars=""
+#    for var in $(yq -r '.workspaces[] | select(.name == '"${workspace}"') | .vars[].name' "${DATA_FILE}"); do
+#      workspace_vars="${workspace_vars} -var ${var}=$(yq -r '.workspaces[] | select(.name == '"${workspace}"') | .vars[] | select(.name == "kubernetes_version") | .value' ${DATA_FILE})"
+#    done
+#
+#    mkdir -p "${LOG_DIR}/${workspace}"
+#    # shellcheck disable=SC2086
+#    TF_WORKSPACE="${workspace}" terraform plan ${workspace_vars} &>"${LOG_DIR}/${workspace}/plan.log" &
+#  done
+}
+
+apply() {
+  # shellcheck disable=SC2086
+  for_workspace_vars_terraform apply ${FLAGS}
+
+#  for workspace in $(yq -r '.workspaces[].name' "${DATA_FILE}"); do
+#    workspace_vars=""
+#    for var in $(yq -r '.workspaces[] | select(.name == '"${workspace}"') | .vars[].name' "${DATA_FILE}"); do
+#      workspace_vars="${workspace_vars} -var ${var}=$(yq -r '.workspaces[] | select(.name == '"${workspace}"') | .vars[] | select(.name == "kubernetes_version") | .value' ${DATA_FILE})"
+#    done
+#
+#    mkdir -p "${LOG_DIR}/${workspace}"
+#    # shellcheck disable=SC2086
+#    TF_WORKSPACE="${workspace}" terraform apply ${FLAGS} ${workspace_vars} &>"${LOG_DIR}/${workspace}/apply.log" &
+#  done
+}
+
+watch_apply() {
+  finished=false
+  while [ "${finished}" != "true" ]; do
+    adder=0
+    for workspace in $(yq -r '.workspaces[].name' "${DATA_FILE}"); do
+      if grep -q "Apply complete" "${LOG_DIR}/${workspace}/apply.log"; then
+        :
+      else
+        echo "Still waiting on workspace: ${workspace}"
+        (( adder++ ))
+      fi
+    done
+    if [ "${adder}" -gt 0 ]; then
+      echo "Apply not complete, continuing to wait..."
+      sleep 5
+    else
+      echo "Apply complete"
+      finished="true"
+    fi
+  done
+}
+
+for_workspace_vars_terraform() {
+  for workspace in $(yq -r '.workspaces[].name' "${DATA_FILE}"); do
+    workspace_vars=""
+    for var in $(yq -r '.workspaces[] | select(.name == '"${workspace}"') | .vars[].name' "${DATA_FILE}"); do
+      workspace_vars="${workspace_vars} -var ${var}=$(yq -r '.workspaces[] | select(.name == '"${workspace}"') | .vars[] | select(.name == "kubernetes_version") | .value' ${DATA_FILE})"
+    done
+
+    mkdir -p "${LOG_DIR}/${workspace}"
+    # shellcheck disable=SC2068,SC2086
+    TF_WORKSPACE="${workspace}" terraform ${@} ${workspace_vars} &>"${LOG_DIR}/${workspace}/${1}.log" &
+  done
+}
+
+check_dependencies() {
+
+  # make log dir
+  mkdir -p "${LOG_DIR}"
+
+  if [ ! -f .terraform.lock.hcl ]; then
+    terraform init
+    for workspace in $(yq -r '.workspaces[].name' "${DATA_FILE}"); do
+      terraform workspace new "${workspace}"
+    done
+  fi
+
+  # check yq
+  if ! command -v yq &> /dev/null; then
+    echo "[ERROR] yq is not installed"
+    exit 1
+  fi
+}
+
+if check_dependencies; then
+  "${@}"
+fi
---
+++ .prow/tf/gcp/variables.tf
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+}
+
+variable "release_channel" {
+  description = "The release channel of the kubernetes version"
+  default     = "RAPID"
+}
+
+variable "name_prefix" {
+  description = "A suffix to append to resource names"
+  default     = "manual"
+}
+
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+  default     = "circonus-dev"
+}
+
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "us-central1"
+}
+
+variable "zones" {
+  type        = list(string)
+  description = "The zone to host the cluster in (required if is a zonal cluster)"
+  default     = ["us-central1-a"]
+}
+
